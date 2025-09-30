9dfa078d45af7533c8d4dae10a23b60bb3ecaf7a
Jorge Kevin Moncayo <nano2916@gmail.com>
2025-09-30 17:05:45 -0300

    Creacion consola monitoreo de sensores ambientales y complemento de clase sensor

---
+++ ConsolaMonitorSensoresAmbientales/Program.cs
@@ -0,0 +1,22 @@
+﻿using MonitorSensoresAmbientales;
+
+namespace ConsolaMonitorSensoresAmbientales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sensor LM35 = new Sensor("LM35", "ºC");
+            Sensor SensorCalidadAire = new Sensor();
+            
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("-----------APP_SENSORES-----------v1.0");
+            Console.WriteLine("--------------------------------------");
+
+            Console.WriteLine(LM35.Datasheet());
+            Console.WriteLine(SensorCalidadAire.Datasheet());
+
+
+        }
+    }
+}
--- MonitorSensoresAmbientales/Sensor.cs
+++ MonitorSensoresAmbientales/Sensor.cs
@@ -1,4 +1,6 @@
-﻿namespace MonitorSensoresAmbientales
+﻿using System.Text;
+
+namespace MonitorSensoresAmbientales
 {
     public class Sensor
     {
@@ -15,7 +17,7 @@
         #region Constructores
 
         static Sensor()
-        { 
+        {
             cantidadSensores = 0;
         }
 
@@ -24,11 +26,144 @@
             cantidadSensores++;
         }
 
-        public Sensor(string nombre, string unidad, double valorActual):this()
+        public Sensor(string nombre, string unidad) : this()
         {
             this.nombre = nombre;
             this.unidad = unidad;
-            this.valorActual = valorActual;
+        }
+
+        #endregion
+
+        #region Propiedades
+
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+            set
+            {
+                if (value != string.Empty)
+                {
+                    this.nombre = value;
+                }
+            }
+        }
+
+        public string Unidad { 
+            get
+            {
+                return this.unidad;
+            }
+            set
+            {
+                if (value != string.Empty)
+                {
+                    this.unidad = value;
+                }
+            }
+        }
+
+        public double ValorActual
+        {
+            get
+            {
+                return this.valorActual;
+            }
+        }
+
+        public double LeerValorTTL
+        {
+            get
+            {
+                this.valorActual = LeerSensor("TTL");
+                return this.valorActual;
+            }
+        }
+
+        public double LeerValorCMOS
+        {
+            get
+            {
+                this.valorActual = LeerSensor("CMOS");
+                return this.ValorActual;
+            }
+        }
+
+        public string CategoriaSensor
+        {
+            get
+            {
+                return CategoriaSensor;
+            }
+            set
+            {
+                if (value != string.Empty)
+                {
+                    CategoriaSensor = value;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Metodos
+
+        /// <summary>
+        /// Muestra la información técnica del sensor
+        /// </summary>
+        /// <returns>Una cadena con él nombre del sensor, la unidad que mide y el valor actual.</returns>
+        public string Datasheet()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"El nombre de este sensor es: {this.nombre}");
+            sb.AppendLine($"La unidad que mide este sensor es: {this.nombre}");
+            sb.AppendLine($"El valor actual de este sensor es: {this.valorActual} {this.unidad}");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Esta función retorna un valor aleatorio entre 0 y un límite superior. El límite superior esta definido por el parametro nivelLogicoSalida.
+        /// </summary>
+        /// <param name="nivelLogicoSalida">Ingrese una cadena con TTL para un nivel lógico de salida entre 0 y 5 V. En su defecto la salida tendrá nivel de salida CMOS, definida entre 0 y 3.3 V</param>
+        /// <returns>Valor ficticio de lectura del sensor definido de acuerdo al tipo de lógica necesaria, ya sea TTL o CMOS.</returns>
+        private double LeerSensor(string nivelLogicoSalida)
+        { 
+            //Este método deberia leer de forma serial a través de un puerto COM el dato que el uC le envíe
+            Random numeroAleatorio = new Random();
+            double valorLeido,valorTTL,valorCMOS,salida;
+            valorLeido = numeroAleatorio.NextDouble();
+            
+            
+            if (nivelLogicoSalida == "TTL")
+            {
+                valorTTL = valorLeido * 5;
+                salida = valorTTL;
+            }
+            else
+            {
+                valorCMOS = valorLeido * 3.3;
+                salida = valorCMOS;
+            }
+            return salida;
+        }
+        /// <summary>
+        /// Verifica si el valor actual está dentro de un rango aceptable definido por el sensor.
+        /// </summary>
+        /// <param name="limiteInferior">Limite inferior del sensor.</param>
+        /// <param name="limiteSuperior">Limite superior del sensor.</param>
+        /// <returns>Retorna un valor booleano True si la lectura del sensor se encuentra dentro de los limites definidos. Caso contrario retorna False.</returns>
+        public bool ValidarLecturaSensor(double limiteInferior, double limiteSuperior)
+        {
+            bool validar = false;
+
+            if (this.valorActual >= limiteInferior && this.valorActual <= limiteSuperior)
+            {
+                validar = true;
+            }
+
+            return validar;
         }
 
 
