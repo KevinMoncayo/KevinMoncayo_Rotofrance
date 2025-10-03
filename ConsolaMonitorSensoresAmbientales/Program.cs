92d71615ac91ca648df6353a7eb8cdbb41691535
KevinMoncayo <nano2916@gmail.com>
2025-10-02 21:18:51 -0300

    Creacion de primera version de menú

--- ConsolaMonitorSensoresAmbientales/Program.cs
+++ ConsolaMonitorSensoresAmbientales/Program.cs
@@ -6,12 +6,11 @@ namespace ConsolaMonitorSensoresAmbientales
     {
         static void Main(string[] args)
         {
+            /*
             Sensor LM35 = new Sensor("LM35", "ºC");
             Sensor SensorCalidadAire = new Sensor();
             
-            Console.WriteLine("--------------------------------------");
-            Console.WriteLine("-----------APP_SENSORES-----------v1.0");
-            Console.WriteLine("--------------------------------------");
+            
             //llamada a método datasheet
             Console.WriteLine(LM35.Datasheet());
             Console.WriteLine(SensorCalidadAire.Datasheet());
@@ -22,10 +21,58 @@ namespace ConsolaMonitorSensoresAmbientales
             Console.WriteLine($"EL sensor {SensorCalidadAire.Nombre} ha sido actualizado!");
 
             //llamada al método LeerSensor() a través de la propiedad LeerValorTTL.
-            LM35.LecturasSensor = LM35.LeerValorTTL;//LecturasSensor debería llamarse AddReadSensor...
 
-            LM35.ValidarLecturaSensor(0, 5);
+            Console.WriteLine($"El nuevo valor del sensor es {LM35.LeerValorTTL}");
 
+            if (LM35.ValidarLecturaSensor(0, 5))
+            {
+                LM35.LecturasSensor = LM35.ValorActual;//LecturasSensor debería llamarse AddReadSensor...
+            }
+
+            //llamada a método datasheet
+            Console.WriteLine(LM35.Datasheet());
+            Console.WriteLine(SensorCalidadAire.Datasheet());
+            */
+            bool continuar = true;
+            int opcionElegida;
+
+            do
+            {
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("-----------APP_SENSORES-----------v1.0");
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("Ingrese una opción del siguiente menú:");
+                Console.WriteLine("\t0) Salir)");
+                Console.WriteLine("\t1) Crear sensor.)");
+                Console.WriteLine("\t2) Leer sensor.)");
+                Console.WriteLine("\t3) Ver historial.)");
+                opcionElegida = int.Parse(Console.ReadLine());
+                while(opcionElegida != 0 && opcionElegida != 1 && opcionElegida != 2 && opcionElegida != 3)
+                {
+                    Console.WriteLine("Opcion incorrecta, ingrese una opción válida!");
+                    opcionElegida = int.Parse(Console.ReadLine());
+                }
+                switch (opcionElegida) 
+                {
+                    case 0:
+                        Console.WriteLine("Saliendo...");
+                        continuar = false;
+                        break;
+                    case 1:
+                        Console.WriteLine("Opcion 1 elegida!");
+                        break;
+                    case 2:
+                        Console.WriteLine("Opcion 2 elegida!");
+                        break;
+                    case 3:
+                        Console.WriteLine("Opcion 3 elegida!");
+                        break;
+                    default:
+                        Console.WriteLine("Estado indeseado...");
+                        break;
+                }
+            }
+            while (continuar);
         }
     }
 }
