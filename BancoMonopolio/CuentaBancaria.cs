7de365d11d87a854f2baa25fbc7d3c17d8404e60
Jorge Kevin Moncayo <nano2916@gmail.com>
2025-10-23 16:50:11 -0300

    Creación proyecto BancoMonopolio y Clase padre CuentaBancaria

---
+++ BancoMonopolio/CuentaBancaria.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace BancoMonopolio
+{
+    public class CuentaBancaria
+    {
+        protected String numeroCuenta;
+        protected String titular;
+        protected double saldo;
+
+        public CuentaBancaria(string numeroCuenta, string titular, double saldo)
+        {
+            this.numeroCuenta = numeroCuenta;
+            this.titular = titular;
+            this.saldo = saldo;
+        }
+
+        public string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Nombre titular: {this.titular}\n");
+            sb.Append($"Numero de cuenta: {this.numeroCuenta}\n");
+            sb.Append($"Saldo: {this.saldo}\n");
+            return sb.ToString();
+        }
+
+        public string Depositar (double monto)
+        {
+            string mensajeSalida;
+            if (monto <= 0)
+            {
+                mensajeSalida = "El monto tiene que ser positivo";
+            }
+            else
+            {
+                this.saldo += monto;
+                mensajeSalida = $"Deposito de ${monto} realizado. Saldo actual: ${this.saldo}";
+            }
+            return mensajeSalida;
+
+
+        }
+
+        public string Retirar (double monto)
+        {
+            string mensajeSalida;
+
+            if (monto <= 0)
+            {
+                mensajeSalida = "El monto tiene que ser positivo";
+            }
+            else
+            {
+                if (monto > this.saldo)
+                {
+                    mensajeSalida = "Saldo insuficiente.";
+                }
+                else
+                {
+                    this.saldo -= monto;
+                    mensajeSalida = $"Extracción de ${monto} exitosa. Saldo actual ${this.saldo}";
+                }
+            }
+            return mensajeSalida;
+        }
+    }
+}
